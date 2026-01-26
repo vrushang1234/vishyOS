3858cd47668c1791fa69effe00309103cbc11275
vrushang1234 <vrushanganand20@gmail.com>
2026-01-25 19:53:06 -0800

    Added frame buffer for printing

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,12 +1,34 @@
 #![no_std]
 
 use core::panic::PanicInfo;
+use limine::request::FramebufferRequest;
+
+mod mem;
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static FB_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 #[unsafe(no_mangle)]
 pub extern "C" fn rust_main() -> ! {
-    loop {}
-}
+    if let Some(framebuffer_response) = FB_REQUEST.get_response() {
+        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
+            let fb_ptr = framebuffer.addr() as *mut u32;
+            let width = framebuffer.pitch() as usize / 4;
 
+            for i in 0..100usize {
+                unsafe {
+                    *fb_ptr.add(i * width + i) = 0xFFFFFFFF;
+                }
+            }
+        }
+    }
+    loop {
+        unsafe {
+            core::arch::asm!("hlt");
+        }
+    }
+}
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
---
+++ kernel/src/mem.rs
@@ -0,0 +1,51 @@
+#![no_std]
+
+use core::ptr;
+
+/// Copy `n` bytes from `src` to `dest`
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        ptr::write(dest.add(i), ptr::read(src.add(i)));
+        i += 1;
+    }
+    dest
+}
+
+/// Set `n` bytes at `dest` to `val`
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memset(dest: *mut u8, val: i32, n: usize) -> *mut u8 {
+    let byte = val as u8;
+    let mut i = 0;
+    while i < n {
+        ptr::write(dest.add(i), byte);
+        i += 1;
+    }
+    dest
+}
+
+/// Compute length of C string (null-terminated)
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn strlen(s: *const u8) -> usize {
+    let mut len = 0;
+    while ptr::read(s.add(len)) != 0 {
+        len += 1;
+    }
+    len
+}
+
+/// Compare n bytes of memory
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memcmp(a: *const u8, b: *const u8, n: usize) -> i32 {
+    let mut i = 0;
+    while i < n {
+        let av = ptr::read(a.add(i));
+        let bv = ptr::read(b.add(i));
+        if av != bv {
+            return av as i32 - bv as i32;
+        }
+        i += 1;
+    }
+    0
+}
